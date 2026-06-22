b62ea5ee0694e42e128f8787457391fe33e6611c
yarik stasin <haholl3000@gmail.com>
2026-06-22 11:37:52 +0500

    Add auto-migrate on startup

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,16 +1,22 @@
-"""
-WSGI config for core project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
+import django
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+django.setup()
+
+# Автоматический прогон миграций при старте контейнера
+from django.core.management import call_command
+try:
+    call_command('migrate', interactive=False)
+    
+    # Автосоздание суперпользователя
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@example.com', '12345678')
+        print("Суперпользователь admin создан успешно!")
+except Exception as e:
+    print(f"Ошибка миграций при старте: {e}")
 
 application = get_wsgi_application()
