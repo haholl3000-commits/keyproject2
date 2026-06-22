ca0a7d037fac3225e1655f77a619b98162811f74
yarik stasin <haholl3000@gmail.com>
2026-06-22 11:29:14 +0500

    Fix ALLOWED_HOSTS

--- core/settings.py
+++ core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,8 @@ SECRET_KEY = 'django-insecure-$)@-&!^x%m8u@l$j2@0z--j5snwrkxxg)=-p(uv^7z8^d8@l!a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# Разрешаем Django работать на хостинге Railway
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,9 +119,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-import os
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Куда переходить после логина
 LOGIN_REDIRECT_URL = 'dashboard'
 
